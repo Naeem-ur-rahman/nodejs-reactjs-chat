a6b04fca1fbb740523a68a2f06fee06f27d23e67
Naeem-ur-rahman <naeemur1001@gmail.com>
2024-01-29 00:21:19 +0500

    rest request + server initial

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const cors = require('cors')
+const PORT = 3001;
+
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: true }));
+app.post('/authenticate', async (req, res) => {
+    const { username } = req.body;
+    return res.json({ username: username, secret: "sha256..." })
+});
+
+app.listen(PORT, () => console.log(`Server Started at http://localhost:${PORT}`));
